a57615adb93a0df3cee468998395d5fcd46e24a7
pavanmanne27698 <112120697+pavanmanne27698@users.noreply.github.com>
2023-06-22 13:46:46 -0500

    change db configuration

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
-const db_host = process.env.DB_HOST;
-const db_pw = process.env.DB_PW;
-const db_user = process.env.DB_USER;
-const db_name = process.env.DB_NAME;
+const db_host = process.env.DB_HOST || "localhost";
+const db_pw = process.env.DB_PW || "pavan@0000";
+const db_user = process.env.DB_USER || "pavan";
+const db_name = process.env.DB_NAME || "online_courier";
 
 module.exports = {
   HOST: db_host,
